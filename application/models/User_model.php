f953f20005fe20e4a9324af6e684bf9034fac155
valvaldy <before1122@gmail.com>
2018-04-26 21:12:17 +0700

    add user model for login

---
+++ application/models/User_model.php
@@ -0,0 +1,48 @@
+<?php
+class User_model extends CI_model{
+
+public function getprofil($id){
+    $this->db->select('*');
+    $this->db->from('user');
+    $this->db->where('user_id',$id);
+    $result=$this->db->get();
+    return $result->row_array();
+}
+
+public function login_user($email,$pass){
+
+  $this->db->select('*');
+  $this->db->from('user');
+  $this->db->where('user_email',$email);
+  $this->db->where('user_password',$pass);
+
+  if($query=$this->db->get())
+  {
+      return $query->row_array();
+  }
+  else{
+    return false;
+  }
+ 
+
+}
+
+public function email_check($email,$user_name){
+
+  $this->db->select('*');
+  $this->db->from('user');
+  $this->db->where('user_email',$email);
+  //$this->db->or();
+  $this->db->or_where('user_name',$user_name);
+  $query=$this->db->get();
+
+  if($query->num_rows()>0){
+    return false;
+  }else{
+    return true;
+  }
+
+}
+
+} 
+?>
