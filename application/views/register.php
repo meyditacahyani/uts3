a03c606b2ea544dfc2af236851d0e4da11e65331
meyditacahyani <meyditadwi.cahyanidewi@yahoo.com>
2018-04-26 19:55:34 +0700

    edit view "register.php"

---
+++ application/views/register.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="<?php blink('assets/css/w3.css') ?>">
+<title>Loneat</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+@font-face {
+    font-family: 'Raleway';s
+    font-style: normal;
+    font-weight: 400;
+    src: local('Raleway'), local('Raleway-Regular'), url(https://fonts.gstatic.com/s/raleway/v12/1Ptug8zYS_SKggPNyCMIT5lu.woff2) format('woff2');
+    unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
+  }
+  /* latin */
+  @font-face {
+    font-family: 'Raleway';
+    font-style: normal;
+    font-weight: 400;
+    src: local('Raleway'), local('Raleway-Regular'), url(https://fonts.gstatic.com/s/raleway/v12/1Ptug8zYS_SKggPNyC0ITw.woff2) format('woff2');
+    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+  }
+    .active {
+    background-color: #c2d6d6;
+    color: #486a6a;
+}
+</style>    
+</head>
+<body class="w3-light-grey">
+
+<!-- w3-content defines a container for fixed size centered content, 
+and is wrapped around the whole page content, except for the footer in this example -->
+<div class="w3-content" style="max-width:1400px">
+
+<!-- Header -->
+<header class="w3-container w3-center w3-padding-32"> 
+  <h1><b>LONEAT</b></h1>
+  <p>Wanna Eat Together</p>
+</header>
+
+<!-- navigation bar -->
+<div class="w3-bar w3-black">
+  <a href="<?php blink('Welcome/index')?>" class="w3-bar-item w3-button w3-mobile">Home</a>
+  <a href="<?php blink('Welcome/login')?>" class="w3-bar-item w3-button w3-mobile">Login</a>
+</div>
+
+<link href="<?php blink('assets/css/bootstrap.css') ?>" rel="stylesheet" type="text/css">
+<script href="<?php blink('assets/js/bootstrap.js') ?>" rel="stylesheet" type="text/javascript"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<span style="background-color:red;">
+  <div class="w3-container"><!-- container class is used to centered  the body of the browser with some decent width-->
+      <div class="w3-row" style="padding:3%;"><!-- row class is used for grid system in Bootstrap-->
+          <div class="col-md-4 col-md-offset-4" style="background: rgba(255, 255, 255, 0.7); margin: 0 auto; float: none;"><!--col-md-4 is used to create the no of colums in the grid also use for medimum and large devices-->
+              <div class="login-panel panel panel-success">
+                  <div class="panel-heading w3-center" style="padding:3%;">
+                      <h3 class="panel-title">Registration</h3>
+                  </div>
+                  <div class="panel-body">
+
+                  <?php
+                  $error_msg=$this->session->flashdata('error_msg');
+                  if($error_msg){
+                    echo $error_msg;
+                  }
+                   ?>
+
+                      <form role="form" method="post" action="<?php echo base_url('user/register_user'); ?>">
+                          <fieldset>
+                              <div class="form-group">
+                                  <input class="form-control" placeholder="Username" name="user_name" type="text" required autofocus>
+                              </div>
+
+                              <div class="form-group">
+                                  <input class="form-control" placeholder="E-mail" name="user_email" type="email" required autofocus>
+                              </div>
+                              <div class="form-group">
+                                  <input class="form-control" placeholder="Password" name="user_password" required type="password" value="">
+                              </div>
+                             
+                              <div class="form-group">
+                                  <input class="form-control" placeholder="Mobile Number" name="user_mobile" required type="text" value="">
+                              </div>
+
+                              <input class="btn btn-lg btn-success btn-block" type="submit" value="Register" name="register" >
+
+                          </fieldset>
+                      </form>
+                      <center><b>Already registered ?</b> <br></b><a href="<?php echo base_url('user/login_view'); ?>">Login here</a></center><!--for centered text-->
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+
+
+
+
+
+</span>
+        <!-- ./container -->
+        <footer class="w3-container w3-dark-grey w3-padding-30 w3-margin-top">
+  <p>Copyright &copy; Loneat Team 2018</p>
+  <p>Universitas Budi Luhur</p>
+</footer>
+</body>
+
+
+</html>
